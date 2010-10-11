f01406336eafe1f3778dc361f9b3be27db255530
nnlight <nnlight@bf0d6b7c-d121-11de-97ae-379497272b75>
2010-10-11 18:51:04 +0000

    .

--- GtkGr/ui.cpp
+++ GtkGr/ui.cpp
@@ -735,19 +735,18 @@ void UIController::ShowAboutDialog()
 		"GNU General Public License v2.\n"
 		"\n"
 		"\n"
-		"This program is free software; you can redistribute it and/or\n"
+		"This program is free software: you can redistribute it and/or\n"
 		"modify it under the terms of the GNU General Public License\n"
-		"as published by the Free Software Foundation; either version 2\n"
+		"as published by the Free Software Foundation, either version 2\n"
 		"of the License, or (at your option) any later version.\n"
 		"\n"
 		"This program is distributed in the hope that it will be useful,\n"
 		"but WITHOUT ANY WARRANTY; without even the implied warranty of\n"
 		"MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the\n"
-		"GNU General Public License for more details\n"
+		"GNU General Public License for more details.\n"
 		"\n"
 		"You should have received a copy of the GNU General Public License\n"
-		"along with this program; if not, write to the Free Software\n"
-		"Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.\n";
+		"along with this program. If not, see <http://www.gnu.org/licenses/>.\n";
 
 	pixbuf = NULL;
 	transparent = NULL;
