42d1335fa8cc7e6eedcce1a12d57b17497494c3c
Nikita S <nnlight@gmail.com>
2015-04-01 14:35:03 +0300

    FindNodeDialog: make initial sort for node list by node label

--- GtkGr/ui_dialogs.cpp
+++ GtkGr/ui_dialogs.cpp
@@ -4,14 +4,6 @@
 
 // коментарий на русском...
 
-typedef struct
-{
-	const gchar *title;
-	const gchar *label;
-	gint   x;
-	gint   y;
-} FindNodeGLSNode;
-
 enum
 {
 	GLS_COLUMN_TITLE,
@@ -49,45 +41,14 @@ create_model( VRGraph *vrg)
 						GLS_COLUMN_Y, node->y_ + node->height_ / 2,
 						-1);
 	}
-	/*for (int i = 0; i < G_N_ELEMENTS (data1); i++)
-	{
-		gtk_list_store_append( store, &iter);
-		gtk_list_store_set(store, &iter,
-						GLS_COLUMN_TITLE, data1[i].title,
-						GLS_COLUMN_LABEL, data1[i].label,
-						GLS_COLUMN_X, data1[i].x,
-						GLS_COLUMN_Y, data1[i].y,
-						-1);
-	}*/
 
+	// сортируем список
+	gtk_tree_sortable_set_sort_column_id( GTK_TREE_SORTABLE(store),
+										  GLS_COLUMN_LABEL,
+										  GTK_SORT_ASCENDING/*GTK_SORT_DESCENDING*/);
 	return GTK_TREE_MODEL( store);
 }
 
-#if 0
-static void
-fixed_toggled_cb( GtkCellRendererToggle *cell,
-	       gchar                 *path_str,
-	       gpointer               data)
-{
-  GtkTreeModel *model = (GtkTreeModel *)data;
-  GtkTreeIter  iter;
-  GtkTreePath *path = gtk_tree_path_new_from_string (path_str);
-  gboolean fixed;
-
-  /* get toggled iter */
-  gtk_tree_model_get_iter (model, &iter, path);
-  gtk_tree_model_get (model, &iter, COLUMN_FIXED, &fixed, -1);
-
-  /* do something with the value */
-  fixed ^= 1;
-
-  /* set new value */
-  gtk_list_store_set (GTK_LIST_STORE (model), &iter, COLUMN_FIXED, fixed, -1);
-
-  /* clean up */
-  gtk_tree_path_free (path);
-}
-#endif
 
 static void
 add_columns( GtkTreeView *treeview)
@@ -96,22 +57,21 @@ add_columns( GtkTreeView *treeview)
 	GtkTreeViewColumn *column;
 	//GtkTreeModel *model = gtk_tree_view_get_model( treeview);
 
-	/* column for fixed toggles */
+	/* column for node titles */
 	renderer = gtk_cell_renderer_text_new();
 	//g_signal_connect( renderer, "toggled",
 	//	G_CALLBACK (fixed_toggled_cb), model);
-
 	column = gtk_tree_view_column_new_with_attributes( "Title",
 									renderer,
 									"text", GLS_COLUMN_TITLE,
 									NULL);
-
 	/* set this column to a fixed sizing (of 50 pixels) */
 	gtk_tree_view_column_set_sizing( GTK_TREE_VIEW_COLUMN(column), GTK_TREE_VIEW_COLUMN_FIXED);
 	gtk_tree_view_column_set_fixed_width( GTK_TREE_VIEW_COLUMN(column), 50);
+	//gtk_tree_view_column_set_sort_column_id( column, GLS_COLUMN_TITLE);
 	gtk_tree_view_append_column( treeview, column);
 
-	/* column for bug numbers */
+	/* column for node labels */
 	renderer = gtk_cell_renderer_text_new();
 	column = gtk_tree_view_column_new_with_attributes( "Label",
 									renderer,
@@ -121,220 +81,52 @@ add_columns( GtkTreeView *treeview)
 	gtk_tree_view_append_column( treeview, column);
 }
 
-
-#if 0
-static GtkWidget *window = NULL;
-
-typedef struct
+/*
+static GtkWidget
+create_tree_view_widget(GtkTreeModel model)
 {
-  const gboolean  fixed;
-  const guint     number;
-  const gchar    *severity;
-  const gchar    *description;
+	GtkWidget *sw;
+	GtkTreeModel *model;
+	GtkWidget *treeview;
+
+	sw = gtk_scrolled_window_new( NULL, NULL);
+	gtk_scrolled_window_set_shadow_type( GTK_SCROLLED_WINDOW(sw), GTK_SHADOW_ETCHED_IN);
+	gtk_scrolled_window_set_policy( GTK_SCROLLED_WINDOW(sw),
+									GTK_POLICY_NEVER,
+									GTK_POLICY_AUTOMATIC);
+
+
+	treeview = gtk_tree_view_new_with_model( model);
+	gtk_tree_view_set_rules_hint( GTK_TREE_VIEW(treeview), TRUE);
+	gtk_tree_view_set_search_column( GTK_TREE_VIEW(treeview), GLS_COLUMN_LABEL);
+	g_signal_connect( treeview, "row-activated", G_CALLBACK(row_activated_cb), uic->m_DrawBuffer.get());
+
+	gtk_container_add( GTK_CONTAINER(sw), treeview);
+
+	// add columns to the tree view
+	add_columns( GTK_TREE_VIEW(treeview));
+
 }
-Bug;
+*/
 
-enum
+static void
+entry_changed_cb( GtkEntry *entry,
+                  gpointer  user_data)
 {
-  COLUMN_FIXED,
-  COLUMN_NUMBER,
-  COLUMN_SEVERITY,
-  COLUMN_DESCRIPTION,
-  NUM_COLUMNS
-};
-
-static Bug data[] =
-{
-  { FALSE, 60482, "Normal",     "scrollable notebooks and hidden tabs" },
-  { FALSE, 60620, "Critical",   "gdk_window_clear_area (gdkwindow-win32.c) is not thread-safe" },
-  { FALSE, 50214, "Major",      "Xft support does not clean up correctly" },
-  { TRUE,  52877, "Major",      "GtkFileSelection needs a refresh method. " },
-  { FALSE, 56070, "Normal",     "Can't click button after setting in sensitive" },
-  { TRUE,  56355, "Normal",     "GtkLabel - Not all changes propagate correctly" },
-  { FALSE, 50055, "Normal",     "Rework width/height computations for TreeView" },
-  { FALSE, 58278, "Normal",     "gtk_dialog_set_response_sensitive () doesn't work" },
-  { FALSE, 55767, "Normal",     "Getters for all setters" },
-  { FALSE, 56925, "Normal",     "Gtkcalender size" },
-  { FALSE, 56221, "Normal",     "Selectable label needs right-click copy menu" },
-  { TRUE,  50939, "Normal",     "Add shift clicking to GtkTextView" },
-  { FALSE, 6112,  "Enhancement","netscape-like collapsable toolbars" },
-  { FALSE, 1,     "Normal",     "First bug :=)" },
-};
-
-static GtkTreeModel *
-create_model (void)
-{
-  gint i = 0;
-  GtkListStore *store;
-  GtkTreeIter iter;
-
-  /* create list store */
-  store = gtk_list_store_new (NUM_COLUMNS,
-			      G_TYPE_BOOLEAN,
-			      G_TYPE_UINT,
-			      G_TYPE_STRING,
-			      G_TYPE_STRING);
-
-  /* add data to the list store */
-  for (i = 0; i < G_N_ELEMENTS (data); i++)
-    {
-      gtk_list_store_append (store, &iter);
-      gtk_list_store_set (store, &iter,
-			  COLUMN_FIXED, data[i].fixed,
-			  COLUMN_NUMBER, data[i].number,
-			  COLUMN_SEVERITY, data[i].severity,
-			  COLUMN_DESCRIPTION, data[i].description,
-			  -1);
-    }
-
-  return GTK_TREE_MODEL (store);
+	const char *text = gtk_entry_get_text( entry);
+	printf( "(change)entry text: %s\n", text);
 }
 
 static void
-fixed_toggled (GtkCellRendererToggle *cell,
-	       gchar                 *path_str,
-	       gpointer               data)
+entry_activate_cb( GtkEntry *entry,
+                   gpointer  user_data)
 {
-  GtkTreeModel *model = (GtkTreeModel *)data;
-  GtkTreeIter  iter;
-  GtkTreePath *path = gtk_tree_path_new_from_string (path_str);
-  gboolean fixed;
+	const char *text = gtk_entry_get_text( entry);
+	printf( "(activate)entry text: %s\n", text);
+	GtkWidget *treeview = (GtkWidget *)user_data;
 
-  /* get toggled iter */
-  gtk_tree_model_get_iter (model, &iter, path);
-  gtk_tree_model_get (model, &iter, COLUMN_FIXED, &fixed, -1);
-
-  /* do something with the value */
-  fixed ^= 1;
-
-  /* set new value */
-  gtk_list_store_set (GTK_LIST_STORE (model), &iter, COLUMN_FIXED, fixed, -1);
-
-  /* clean up */
-  gtk_tree_path_free (path);
+	gtk_widget_grab_focus( treeview);
 }
-
-static void
-add_columns (GtkTreeView *treeview)
-{
-  GtkCellRenderer *renderer;
-  GtkTreeViewColumn *column;
-  GtkTreeModel *model = gtk_tree_view_get_model (treeview);
-
-  /* column for fixed toggles */
-  renderer = gtk_cell_renderer_toggle_new ();
-  g_signal_connect (renderer, "toggled",
-		    G_CALLBACK (fixed_toggled), model);
-
-  column = gtk_tree_view_column_new_with_attributes ("Fixed?",
-						     renderer,
-						     "active", COLUMN_FIXED,
-						     NULL);
-
-  /* set this column to a fixed sizing (of 50 pixels) */
-  gtk_tree_view_column_set_sizing (GTK_TREE_VIEW_COLUMN (column),
-				   GTK_TREE_VIEW_COLUMN_FIXED);
-  gtk_tree_view_column_set_fixed_width (GTK_TREE_VIEW_COLUMN (column), 50);
-  gtk_tree_view_append_column (treeview, column);
-
-  /* column for bug numbers */
-  renderer = gtk_cell_renderer_text_new ();
-  column = gtk_tree_view_column_new_with_attributes ("Bug number",
-						     renderer,
-						     "text",
-						     COLUMN_NUMBER,
-						     NULL);
-  gtk_tree_view_column_set_sort_column_id (column, COLUMN_NUMBER);
-  gtk_tree_view_append_column (treeview, column);
-
-  /* column for severities */
-  renderer = gtk_cell_renderer_text_new ();
-  column = gtk_tree_view_column_new_with_attributes ("Severity",
-						     renderer,
-						     "text",
-						     COLUMN_SEVERITY,
-						     NULL);
-  gtk_tree_view_column_set_sort_column_id (column, COLUMN_SEVERITY);
-  gtk_tree_view_append_column (treeview, column);
-
-  /* column for description */
-  renderer = gtk_cell_renderer_text_new ();
-  column = gtk_tree_view_column_new_with_attributes ("Description",
-						     renderer,
-						     "text",
-						     COLUMN_DESCRIPTION,
-						     NULL);
-  gtk_tree_view_column_set_sort_column_id (column, COLUMN_DESCRIPTION);
-  gtk_tree_view_append_column (treeview, column);
-}
-
-GtkWidget *
-do_list_store (GtkWidget *do_widget)
-{
-  if (!window)
-    {
-      GtkWidget *vbox;
-      GtkWidget *label;
-      GtkWidget *sw;
-      GtkTreeModel *model;
-      GtkWidget *treeview;
-
-      /* create window, etc */
-      window = gtk_window_new (GTK_WINDOW_TOPLEVEL);
-      gtk_window_set_screen (GTK_WINDOW (window),
-			     gtk_widget_get_screen (do_widget));
-      gtk_window_set_title (GTK_WINDOW (window), "GtkListStore demo");
-
-      g_signal_connect (window, "destroy",
-			G_CALLBACK (gtk_widget_destroyed), &window);
-      gtk_container_set_border_width (GTK_CONTAINER (window), 8);
-
-      vbox = gtk_vbox_new (FALSE, 8);
-      gtk_container_add (GTK_CONTAINER (window), vbox);
-
-      label = gtk_label_new ("This is the bug list (note: not based on real data, it would be nice to have a nice ODBC interface to bugzilla or so, though).");
-      gtk_box_pack_start (GTK_BOX (vbox), label, FALSE, FALSE, 0);
-
-      sw = gtk_scrolled_window_new (NULL, NULL);
-      gtk_scrolled_window_set_shadow_type (GTK_SCROLLED_WINDOW (sw),
-					   GTK_SHADOW_ETCHED_IN);
-      gtk_scrolled_window_set_policy (GTK_SCROLLED_WINDOW (sw),
-				      GTK_POLICY_NEVER,
-				      GTK_POLICY_AUTOMATIC);
-      gtk_box_pack_start (GTK_BOX (vbox), sw, TRUE, TRUE, 0);
-
-      /* create tree model */
-      model = create_model( NULL);
-
-      /* create tree view */
-      treeview = gtk_tree_view_new_with_model (model);
-      gtk_tree_view_set_rules_hint (GTK_TREE_VIEW (treeview), TRUE);
-      gtk_tree_view_set_search_column (GTK_TREE_VIEW (treeview),
-				       COLUMN_DESCRIPTION);
-
-      g_object_unref (model);
-
-      gtk_container_add (GTK_CONTAINER (sw), treeview);
-
-      /* add columns to the tree view */
-      add_columns (GTK_TREE_VIEW (treeview));
-
-      /* finish & show */
-      gtk_window_set_default_size (GTK_WINDOW (window), 280, 250);
-    }
-
-  if (!GTK_WIDGET_VISIBLE (window))
-    gtk_widget_show_all (window);
-  else
-    {
-      gtk_widget_destroy (window);
-      window = NULL;
-    }
-
-  return window;
-}
-#endif
-
 
 static void
 row_activated_cb( GtkTreeView       *tree_view,
@@ -355,6 +147,16 @@ row_activated_cb( GtkTreeView       *tree_view,
 						GLS_COLUMN_Y, &y,
 						-1);
 	db->CenterVisibleAreaByVrgCoords( x, y);
+}
+
+static gboolean
+cursor_on_match_cb(GtkEntryCompletion *widget,
+               GtkTreeModel       *model,
+               GtkTreeIter        *iter,
+               gpointer            user_data)
+{
+	printf("cursor_on_match_cb\n");
+	return FALSE;
 }
 
 static gboolean
@@ -393,16 +195,14 @@ void UiShowFindNodeDialog( UIController *uic)
 	g_signal_connect( dialog, "destroy", G_CALLBACK(gtk_widget_destroyed), &dialog);
 	vbox = GTK_DIALOG(dialog)->vbox;
 
-	/* Create our entry */
+	/* Create entry with completion */
 	entry = gtk_entry_new();
 	gtk_box_pack_start( GTK_BOX(vbox), entry, FALSE, FALSE, 0);
-	/* Create the completion object */
-	completion = gtk_entry_completion_new ();
-	/* Assign the completion to the entry */
-	gtk_entry_set_completion (GTK_ENTRY (entry), completion);
-	g_object_unref (completion);
+	completion = gtk_entry_completion_new();
+	gtk_entry_set_completion( GTK_ENTRY(entry), completion);
+	g_object_unref( completion);
 
-	
+
 	label = gtk_label_new( "Select node from the list bellow. Main window will ceterized by double click.");
 	gtk_box_pack_start( GTK_BOX(vbox), label, FALSE, FALSE, 0);
 
@@ -427,25 +227,27 @@ void UiShowFindNodeDialog( UIController *uic)
 	/* add columns to the tree view */
 	add_columns( GTK_TREE_VIEW(treeview));
 
+	/* добавляем модель к completion, указываем по какому столбцу работать */
 	gtk_entry_completion_set_model( completion, model);
-	/* Use model column 0 as the text column */
 	gtk_entry_completion_set_text_column( completion, GLS_COLUMN_LABEL);
 
 	g_object_unref( model);
 
 
+	/*g_signal_connect( entry, "changed", G_CALLBACK(entry_changed_cb), NULL);
+	g_signal_connect( entry, "activate", G_CALLBACK(entry_activate_cb), treeview);
+
+	g_signal_connect( completion, "cursor-on-match", G_CALLBACK(cursor_on_match_cb), NULL);*/
 	g_signal_connect( completion, "match-selected", G_CALLBACK(match_selected_cb), uic->m_DrawBuffer.get());
 
 	/* finish & show */
-	//gtk_window_set_default_size( GTK_WINDOW(window), 280, 250);
 	gtk_window_set_default_size( GTK_WINDOW(dialog), 280, 250);
 	//gtk_window_set_default_size( GTK_WINDOW(dialog), 450, 500);
     
-	//gtk_widget_show_all( window);
 	gtk_widget_show_all( dialog);
 
 	/*int response = gtk_dialog_run( GTK_DIALOG(dialog));
-	gtk_widget_destroy( dialog);
+	//gtk_widget_destroy( dialog);
 	if ( response == GTK_RESPONSE_YES )
 	{
 		printf("YES\n");
@@ -453,6 +255,5 @@ void UiShowFindNodeDialog( UIController *uic)
 	{
 		printf("~NO\n");
 	}*/
-	//gtk_widget_show( dialog);
 }
 
